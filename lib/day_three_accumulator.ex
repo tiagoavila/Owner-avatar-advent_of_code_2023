93047a8c2044bd6232a9c8a7db68b374ebeae070
Tiago Avila <tiago.avila09@gmail.com>
2023-12-06 09:58:22 -0300

    day three part one and wip part two

--- lib/day_three_accumulator.ex
+++ lib/day_three_accumulator.ex
@@ -1,8 +1,3 @@
 defmodule DayThree.Accumulator do
-  defstruct numbers: [],
-            valid_symbols: %{},
-            previous: "",
-            is_previous_a_valid_symbol: false,
-            is_previous_a_number: false,
-            add_number_as_valid: false
+  defstruct numbers: [], valid_symbols: %MapSet{}
 end
--- test/day_three_test.exs
+++ test/day_three_test.exs
@@ -6,4 +6,17 @@ defmodule DayThreeTest do
            |> String.split("\r\n", trim: true)
            |> DayThree.part_one() == 4361
   end
+
+  test "Day 03 - challenge part one" do
+    assert File.read!("./inputs/day_three/challenge_input.txt")
+           |> String.split("\r\n", trim: true)
+           |> DayThree.part_one()
+           |> IO.inspect(label: "Day 02 - challenge one")
+  end
+
+  test "Day 03 - test part two" do
+    assert File.read!("./inputs/day_three/test_input.txt")
+           |> String.split("\r\n", trim: true)
+           |> DayThree.part_two() == 467_835
+  end
 end
