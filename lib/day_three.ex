ce453648440bd246ca09b348947be8fe59e43224
Tiago Avila <tiago.avila09@gmail.com>
2023-12-06 09:58:33 -0300

    wip part 2

--- lib/day_three.ex
+++ lib/day_three.ex
@@ -1,101 +1,79 @@
 defmodule DayThree do
   alias DayThree.{Accumulator, NumberPosition}
 
-  @invalid_symbols ~w(. 0 1 2 3 4 5 6 7 8 9)
-  @numbers ~w(0 1 2 3 4 5 6 7 8 9)
+  @valid_symbol_regex ~r/[^\d|^.]/
+  @numbers_regex ~r/\d+/
 
   def part_one(input) do
-    input
-    |> Enum.with_index()
-    |> Enum.flat_map(fn {row, row_index} ->
-      row
-      |> String.codepoints()
+    %{valid_symbols: valid_symbols, numbers: numbers} =
+      input
       |> Enum.with_index()
-      |> Enum.map(fn {char, col_index} ->
-        {row_index, col_index, char}
-      end)
-    end)
-    |> Enum.reduce(%Accumulator{}, fn {row_index, col_index, char},
-                                      %{is_previous_a_valid_symbol: is_previous_a_valid_symbol} =
-                                        acc ->
-      case char |> get_char_type() do
-        :valid_symbol when acc.is_previous_a_number ->
-          %{
-            acc
-            | valid_symbols: Map.put(acc.valid_symbols, {row_index, col_index}, char),
-              is_previous_a_valid_symbol: true,
-              is_previous_a_number: false,
-              previous: "",
-              numbers: [
-                %NumberPosition{number: String.to_integer(acc.previous), is_part_number: true}
-                | acc.numbers
-              ]
-          }
+      |> Enum.reduce(%Accumulator{}, fn {row, row_index}, acc ->
+        valid_symbols =
+          Regex.scan(@valid_symbol_regex, row, return: :index)
+          |> Enum.reduce(%MapSet{}, fn [{col_index, _}], mapset_acc ->
+            MapSet.put(mapset_acc, {row_index, col_index})
+          end)
 
-        :valid_symbol ->
-          %{
-            acc
-            | valid_symbols: Map.put(acc.valid_symbols, {row_index, col_index}, char),
-              is_previous_a_valid_symbol: true,
-              is_previous_a_number: false,
-              previous: ""
-          }
+        numbers_updated =
+          Regex.scan(@numbers_regex, row, return: :index)
+          |> Enum.reduce(acc.numbers, fn [{col_index, size}], numbers_acc ->
+            is_part_number =
+              MapSet.member?(valid_symbols, {row_index, col_index - 1}) or
+                MapSet.member?(valid_symbols, {row_index, col_index + size})
 
-        :number
-        when acc.is_previous_a_number == false and acc.is_previous_a_valid_symbol == false ->
-          %{
-            acc
-            | previous: char,
-              is_previous_a_number: true
-          }
+            [
+              %NumberPosition{
+                row: row_index,
+                init_col: col_index,
+                end_col: col_index + size - 1,
+                number: String.to_integer(String.slice(row, col_index, size)),
+                is_part_number: is_part_number
+              }
+              | numbers_acc
+            ]
+          end)
 
-        :number when acc.is_previous_a_number == true ->
-          %{
-            acc
-            | previous: acc.previous <> char,
-              is_previous_a_number: true,
-              is_previous_a_valid_symbol: false
-          }
-
-        :number when acc.is_previous_a_valid_symbol == true ->
-          %{
-            acc
-            | add_number_as_valid: true,
-              is_previous_a_number: true,
-              is_previous_a_valid_symbol: false,
-              previous: char
-          }
-
-        :invalid_symbol when acc.is_previous_a_number == true ->
-          %{
-            acc
-            | numbers: [%NumberPosition{number: String.to_integer(acc.previous), is_part_number: acc.add_number_as_valid} | acc.numbers],
-              is_previous_a_number: false,
-							add_number_as_valid: false,
-              previous: ""
-          }
-
-        _ ->
+        %{
           acc
-      end
-    end)
-    |> IO.inspect(label: "Day 03 - challenge one")
+          | valid_symbols: MapSet.union(acc.valid_symbols, valid_symbols),
+            numbers: numbers_updated
+        }
+      end)
 
-    # |> Map.new(fn {row_index, col_index, char} ->
-    # 	{{row_index, col_index}, char}
-    # end)
-
-    4361
+    numbers
+    |> Enum.reduce([], &check_valid_numbers(&1, &2, valid_symbols))
+    |> Enum.filter(&(&1 != nil))
+    |> Enum.sum()
   end
 
-  defp get_char_type(char) do
-    cond do
-      char |> is_valid_symbol?() -> :valid_symbol
-      char |> is_number?() -> :number
-      true -> :invalid_symbol
+  def part_two(input) do
+
+    467835
+  end
+
+  defp check_valid_numbers(%NumberPosition{is_part_number: true, number: number}, acc, _) do
+    [number | acc]
+  end
+
+  defp check_valid_numbers(
+         %NumberPosition{row: row, init_col: init_col, end_col: end_col, number: number},
+         acc,
+         valid_symbols
+       ) do
+    is_valid =
+      Enum.reduce_while((init_col - 1)..(end_col + 1), false, fn col_index, _ ->
+        if MapSet.member?(valid_symbols, {row - 1, col_index}) or
+             MapSet.member?(valid_symbols, {row + 1, col_index}) do
+          {:halt, true}
+        else
+          {:cont, false}
+        end
+      end)
+
+    case is_valid do
+      true -> [number | acc]
+      false -> [nil | acc]
     end
   end
-
-  defp is_valid_symbol?(char), do: char not in @invalid_symbols
-  defp is_number?(char), do: char in @numbers
 end
