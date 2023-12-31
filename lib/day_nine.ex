741ea6716a93ceac645657352ce5ae916d313911
Tiago Avila <tiago.avila09@gmail.com>
2023-12-31 12:04:58 -0300

    day nine part 1 - small refactoring

--- lib/day_nine.ex
+++ lib/day_nine.ex
@@ -1,7 +1,7 @@
 defmodule DayNine do
 	def part_one(input) do
 	 input
-	 |> Enum.reduce(0, fn line, acc -> acc + generate_next_value(line) end)
+	 |> Enum.reduce(0, &generate_next_value(&1) + &2)
 	end
 
 	def generate_next_value(line_input) do
@@ -12,14 +12,14 @@ defmodule DayNine do
 	end
 
 	def extrapolate(history_line) do
-		differences = generate_difference_sequency(history_line)
+		differences = generate_differences_sequency(history_line)
 		case Enum.all?(differences, &(&1 == 0)) do
 			true -> List.last(history_line)
 			false -> List.last(history_line) + extrapolate(differences)
 		end
 	end
 
-	def generate_difference_sequency(history_line) do
+	def generate_differences_sequency(history_line) do
 		history_line
 		|> Enum.chunk_every(2, 1, :discard)
 		|> Enum.map(fn [a, b] -> b - a end)
