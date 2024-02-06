1f5580cbc4b6d2fa13f8a7f6f8c290a8d007948c
Tiago Avila <tiago.avila09@gmail.com>
2024-02-06 09:35:25 -0300

    day 18 part 2 WIP

--- lib/day_eighteen.ex
+++ lib/day_eighteen.ex
@@ -2,33 +2,59 @@ defmodule DayEighteen do
   @directions [{-1, 0}, {0, -1}, {1, 0}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}]
 
   def part_one(input) do
+    input
+    |> Enum.map(fn line ->
+      [dir, meters, _] = String.split(line, " ", trim: true)
+      {dir, String.to_integer(meters)}
+    end)
+    |> dig()
+  end
+
+  # https://elixirforum.com/t/advent-of-code-2023-day-18/60436/10
+  # https://www.101computing.net/the-shoelace-algorithm/
+  # https://www.youtube.com/watch?v=0KjG8Pg6LGk
+  def part_two(input) do
+    input
+    |> Enum.map(fn line ->
+      <<hex_value::binary-size(5), dir_char::binary>> = Regex.replace(~r/^.+\(#(\w+)\)/, line, "\\1")
+
+      {meters, _} = Integer.parse(hex_value, 16)
+
+      case dir_char do
+        "0" -> {"R", meters}
+        "1" -> {"D", meters}
+        "2" -> {"L", meters}
+        "3" -> {"U", meters}
+      end
+    end)
+    |> dig()
+  end
+
+  defp dig(input) do
     dig_map = Map.new() |> Map.put({0, 0}, "#")
 
     input
-    |> Enum.reduce({dig_map, {0, 0}}, fn line, {dig_map, {last_row, last_col}} ->
-      [dir, meters, _] = String.split(line, " ", trim: true)
-      parsed_meters = String.to_integer(meters)
-
+    |> Enum.reduce({dig_map, {0, 0}}, fn {dir, meters}, {dig_map, {last_row, last_col}} ->
       case dir do
         "R" ->
-          1..parsed_meters
+          1..meters
           |> Enum.into(dig_map, fn i -> {{last_row, last_col + i}, "#"} end)
-          |> then(fn new_map -> {new_map, {last_row, last_col + parsed_meters}} end)
+          |> then(fn new_map -> {new_map, {last_row, last_col + meters}} end)
 
         "L" ->
-          1..parsed_meters
+          1..meters
           |> Enum.into(dig_map, fn i -> {{last_row, last_col - i}, "#"} end)
-          |> then(fn new_map -> {new_map, {last_row, last_col - parsed_meters}} end)
+          |> then(fn new_map -> {new_map, {last_row, last_col - meters}} end)
 
         "D" ->
-          1..parsed_meters
+          1..meters
           |> Enum.into(dig_map, fn i -> {{last_row + i, last_col}, "#"} end)
-          |> then(fn new_map -> {new_map, {last_row + parsed_meters, last_col}} end)
+          |> then(fn new_map -> {new_map, {last_row + meters, last_col}} end)
 
         "U" ->
-          1..parsed_meters
+          1..meters
           |> Enum.into(dig_map, fn i -> {{last_row - i, last_col}, "#"} end)
-          |> then(fn new_map -> {new_map, {last_row - parsed_meters, last_col}} end)
+          |> then(fn new_map -> {new_map, {last_row - meters, last_col}} end)
       end
     end)
     |> then(fn {dig_map, _} ->
@@ -42,9 +68,6 @@ defmodule DayEighteen do
     end)
     |> Map.keys()
     |> Enum.count()
-  end
-
-  def part_two(input) do
   end
 
   defp flood_fill([], dig_map), do: dig_map
--- test/day_eighteen_test.exs
+++ test/day_eighteen_test.exs
@@ -14,11 +14,11 @@ defmodule DayEighteenTest do
            |> IO.inspect(label: "DayEighteen - challenge one")
 	end
 
-	# test "DayEighteen - test part two" do
-	# 	assert File.read!("./inputs/day_eighteen/test_input.txt")
-	# 	   |> String.split("\r\n", trim: true)
-	# 	   |> DayEighteen.part_two() == 0
-	# end
+	test "DayEighteen - test part two" do
+		assert File.read!("./inputs/day_eighteen/test_input.txt")
+		   |> String.split("\r\n", trim: true)
+		   |> DayEighteen.part_two() == 952_408_144_115
+	end
 
 	#test "DayEighteen - challenge part two" do
 	#	assert File.read!("./inputs/day_eighteen/challenge_input.txt")
